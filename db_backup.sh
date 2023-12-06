dc50687d5920f26111f2466ac0f73d72df977f43
LCSC VM <langaracompsciclub@gmail.com>
2023-12-06 14:31:13 -0800

    many changes

--- db_backup.sh
+++
@@ -1,3 +0,0 @@
-docker exec -t postgres -c -U postgres > PG_DB_dump_`date +%d-%m-%Y"_"%H_%M_%S`.sql
-
-
--- db_restore.sh
+++
@@ -1,2 +0,0 @@
-# implementation is left as an exercise to the reader
-cat your_dump.sql | docker exec -i postgres psql -U your-db-user -d your-db-name
