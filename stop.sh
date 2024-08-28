739323771439ec087d37d70be3209e9417ff7042
LCSC VM <langaracompsciclub@gmail.com>
2024-08-27 21:21:51 -0700

    make stop.sh actually stop things

--- stop.sh
+++ stop.sh
@@ -1,1 +1,24 @@
+
+# start main docker compose
 docker compose stop
+
+# start plausible
+cd plausible/
+docker compose stop
+cd ..
+
+# start findblahaj
+cd secret/
+docker compose stop
+cd ..
+
+#start hackathon portal
+cd lhacks-portal
+
+cd backend
+docker compose stop
+cd ..
+
+cd frontend
+docker compose stop
+cd ..
